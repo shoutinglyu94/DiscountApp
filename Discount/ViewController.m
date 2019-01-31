6b86c335f487e2acc82c22266d1d9ed3b730c4e7
Shouting Lyu <lyu.s@husky.neu.edu>
2019-01-30 22:58:44 -0800

    DiscountApp Complete

--- Discount/ViewController.m
+++ Discount/ViewController.m
@@ -9,15 +9,43 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *inputTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *controlPad;
+@property (weak, nonatomic) IBOutlet UILabel *savedAmount;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
 
 @end
 
 @implementation ViewController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)didTapControlPad:(id)sender {
+    [self updateValue];
+}
+
+- (IBAction)didTapScreen:(id)sender {
+    [self updateValue];
+}
+
+- (void)updateValue
+{
+    //
+    [self.view endEditing:YES];
+    
+    CGFloat billAmount = [self.inputTextField.text floatValue];
+    
+    NSArray *discountRate = @[@(0.9),@(0.8),@(0.7)];
+    
+    CGFloat savedPrice = billAmount * (1 - [discountRate[[self.controlPad selectedSegmentIndex]] floatValue]);
+    CGFloat totalPrice = billAmount * [discountRate[[self.controlPad selectedSegmentIndex]] floatValue];
+    
+    self.savedAmount.text = [NSString stringWithFormat:@"$%10.2f",savedPrice];
+    self.totalAmount.text = [NSString stringWithFormat:@"$%10.2f",totalPrice];
+}
 
 @end
